908d5dde41f85915edeb4c9af33cb64e841b5dca
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-25 15:18:56 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- interface/if_OS_PRG260_Keystore.h
+++ interface/if_OS_PRG260_Keystore.h
@@ -1,7 +1,11 @@
 /*
- *  Rng_Prg260 Keystore Defines
+ * Rng_Prg260 Keystore Defines
  *
- *  Copyright (C) 2023, HENSOLDT Cyber GmbH
+ * Copyright (C) 2023-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #pragma once
